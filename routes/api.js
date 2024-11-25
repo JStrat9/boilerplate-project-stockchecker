a7c05fd87e19eb5c41dc128b6c9b283165056baa
joanl <joanlapaix.d@gmail.com>
2024-11-25 08:38:30 +0100

    Initial commit

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,7 @@
-'use strict';
+"use strict";
 
 module.exports = function (app) {
+    // https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/TSLA/quote
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+    app.route("/api/stock-prices").get(function (req, res) {});
 };
